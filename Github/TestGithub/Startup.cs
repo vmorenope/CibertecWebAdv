14f43412b28409a7ea743762943f2b671e46b42a
Juvega <NetAdv-VS@S000CVI00>
2017-09-16 09:54:52 -0500

    Creación de proyecto MVC

---
+++ Github/TestGithub/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TestGithub.Startup))]
+namespace TestGithub
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
